00c355377d392241ec3c4b09986c7eca0af995ab
erick luiz <si.erickluiz@gmail.com>
2019-05-15 23:26:01 -0300

    Add Scheduler from to computer the month winner

--- Schedule/startSchedules.js
+++ Schedule/startSchedules.js
@@ -1,8 +1,55 @@
 
-var schedule = require('node-schedule');
- 
-var j = schedule.scheduleJob('36 * * * *', function(){
-  console.log('The answer to life, the universe, and everything!');
+const schedule = require('node-schedule');
+const Cone = require('../models/Cone')
+const WinnerMonth = require('../models/WinnerMonth')
+
+// * segundo (opcional)   *  minuto  *  hora  * dia do mês   * mês   * dia da semana
+var j = schedule.scheduleJob('30 1 1 * *', function(){
+	getWinnerCone();
 });
-console.log('Tkdfjkdjfdsjlf')
+
+
+const getWinnerCone = () => Cone.find({}, (err, cones) => {
+	if(err) return;
+	let currentMonth = new Date().getMonth();
+	let passadMonth  = currentMonth == 0? 11: currentMonth - 1; 
+	let max = 0;
+	let winners = [];
+	
+	cones.forEach(c => {
+		let conePoints = countPointsMonth(c.points, passadMonth);
+		if(conePoints < max) return;
+
+		if(conePoints > max){
+			max = conePoints;
+			winners = [];
+		}
+		
+		winners.push(c);
+	});
+
+	registerWinners(winners, max);
+	console.log(winners);
+});
+
+const countPointsMonth = (point, passadMonth) => {
+	return point.filter(p => new Date(p.date).getMonth() == passadMonth).length
+}
+
+const registerWinners = (winners, points) => {
+
+	let winnerMonth = new WinnerMonth({
+		dateMonth: new Date().getMonth(),
+		dateYear: new Date().getFullYear(), 
+		points: points,
+		cones: winners
+	});
+
+	winnerMonth.save(function(err, winnerMonth) {
+        if(err) return; 
+        console.log(winnerMonth);
+    });				
+}
+
+console.log('\nAtivando scheduler de calculo de vencedor\n')
 module.exports = schedule
